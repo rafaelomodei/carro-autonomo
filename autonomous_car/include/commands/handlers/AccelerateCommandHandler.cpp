24cd4ec13abb76c4a02d59081ecba61b58422ccf
rafaelomodei <rafael.omodei@outlook.com>
2025-01-19 07:28:37 -0300

    feat: move vehicle

--- autonomous_car/include/commands/handlers/AccelerateCommandHandler.cpp
+++ autonomous_car/include/commands/handlers/AccelerateCommandHandler.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <pigpio.h>
 
+// Definições dos pinos GPIO
+constexpr int PIN_FORWARD  = 23; // GPIO para direção Forward
+constexpr int PIN_BACKWARD = 24; // GPIO para direção Backward
+constexpr int PIN_PWM      = 18; // GPIO para controle de velocidade (PWM)
+
 void AccelerateCommandHandler::handle(const rapidjson::Value &cmd) const {
   setupGPIO();
 
@@ -9,9 +14,12 @@ void AccelerateCommandHandler::handle(const rapidjson::Value &cmd) const {
     int speed = cmd["speed"].GetInt();
     std::cout << "Comando recebido: Acelerar para " << speed << " km/h" << std::endl;
 
+    setMotorDirection(speed);
+
     // Converter velocidade para PWM (0-255)
-    int pwmValue = (speed * 255) / 100; // Assumindo 100 km/h como velocidade máxima
-    gpioPWM(18, pwmValue);              // Configura PWM no pino 18
+    int pwmValue = abs(speed * 255 / 100);  // Assumindo 100 km/h como velocidade máxima
+    pwmValue     = std::min(pwmValue, 255); // Garantir que não ultrapasse o máximo permitido
+    gpioPWM(PIN_PWM, pwmValue);             // Configura o PWM para o pino de velocidade
   } else {
     std::cerr << "Comando 'accelerate' inválido: falta 'speed'." << std::endl;
   }
@@ -24,7 +32,23 @@ void AccelerateCommandHandler::setupGPIO() const {
       std::cerr << "Falha ao inicializar pigpio." << std::endl;
       exit(1);
     }
-    gpioSetMode(18, PI_OUTPUT); // Configura o pino 18 como saída
+    gpioSetMode(PIN_FORWARD, PI_OUTPUT);  // Configura pino Forward como saída
+    gpioSetMode(PIN_BACKWARD, PI_OUTPUT); // Configura pino Backward como saída
+    gpioSetMode(PIN_PWM, PI_OUTPUT);      // Configura pino PWM como saída
+    gpioPWM(PIN_PWM, 0);                  // Inicializa PWM com 0% de duty cycle
     isSetup = true;
+  }
+}
+
+void AccelerateCommandHandler::setMotorDirection(int speed) const {
+  if (speed > 0) {
+    gpioWrite(PIN_FORWARD, PI_HIGH); // Ativa direção Forward
+    gpioWrite(PIN_BACKWARD, PI_LOW); // Desativa direção Backward
+  } else if (speed < 0) {
+    gpioWrite(PIN_FORWARD, PI_LOW);   // Desativa direção Forward
+    gpioWrite(PIN_BACKWARD, PI_HIGH); // Ativa direção Backward
+  } else {
+    gpioWrite(PIN_FORWARD, PI_LOW); // Desativa ambas as direções
+    gpioWrite(PIN_BACKWARD, PI_LOW);
   }
 }
