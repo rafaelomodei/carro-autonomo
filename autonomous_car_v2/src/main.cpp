7e67262b3debada327bf3c334358f9b197f5ccf0
Rafael Omodei <rafael_omodei007@hotmail.com>
2025-06-27 01:34:36 -0300

    Add video streaming and config to v2

--- autonomous_car_v2/src/main.cpp
+++ autonomous_car_v2/src/main.cpp
@@ -1,4 +1,6 @@
 #include "managers/WebSocketManager.h"
+#include "config/VehicleConfig.h"
+#include "video/VideoStreamHandler.h"
 #include <atomic>
 #include <chrono>
 #include <csignal>
@@ -17,12 +19,21 @@ int main() {
         std::cout << "Received: " << msg << std::endl;
     });
 
+    VehicleConfig &config = VehicleConfig::getInstance();
+    (void)config; // apenas para evitar aviso se não usado
+
     std::thread wsThread([&] { ws.start(); });
+
+    VideoStreamHandler video(0, [&](const std::string &frame) {
+        ws.sendFrame(frame);
+    });
+    video.startStreaming();
 
     while (running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
+    video.stopStreaming();
     wsThread.join();
     return 0;
 }
