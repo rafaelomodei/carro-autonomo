3decd99310a193427b4ff840a1833bbebbf52c28
rafaelomodei <rafael.omodei@outlook.com>
2026-04-08 19:51:38 -0300

    refactor: removed version deprecied

--- autonomous_car/include/commands/handlers/AlertCommandHandler.cpp
+++
@@ -1,6 +0,0 @@
-#include "AlertCommandHandler.h"
-#include <iostream>
-
-void AlertCommandHandler::handle(const rapidjson::Value &) const {
-  std::cout << "Comando recebido: Ligar alerta" << std::endl;
-}
--- autonomous_car_v2/include/commands/handlers/BrakeCommandHandler.cpp
+++
@@ -1,6 +0,0 @@
-#include "BrakeCommandHandler.h"
-#include <iostream>
-
-void BrakeCommandHandler::handle(const rapidjson::Value &) const {
-  std::cout << "Comando recebido: Frear" << std::endl;
-}
